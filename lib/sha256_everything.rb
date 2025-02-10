5cf4ebbe2c5adb94c6a67c9a8f4fbaff523e0b4f
Jibril Tapiador <tapiador@jib.is>
2025-02-11 03:48:46 +0800

    feat: add main library file (lib/sha256_everything.rb) to require core classes

--- lib/sha256_everything.rb
+++ lib/sha256_everything.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "sha256_everything/version"
+require_relative 'sha256_everything/version'
+
+require 'sha256_everything/version'
+require 'sha256_everything/cli'
+require 'sha256_everything/application'
+require 'sha256_everything/file_collector'
+require 'sha256_everything/file_hasher'
+require 'sha256_everything/tree_generator'
+require 'sha256_everything/output_writer'
 
 module Sha256Everything
   class Error < StandardError; end
