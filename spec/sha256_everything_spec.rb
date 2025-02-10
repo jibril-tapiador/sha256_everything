f93004e02a4c5a0121c37cbbeee90170a589462c
Jibril Tapiador <tapiador@jib.is>
2025-02-11 04:56:03 +0800

    test: remove placeholder spec and update quoting style in sha256_everything_spec.rb

--- spec/sha256_everything_spec.rb
+++ spec/sha256_everything_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Sha256Everything do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Sha256Everything::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
