64ab2672216bbedd72c321101ef6c05d98917b45
Jibril Tapiador <tapiador@jib.is>
2025-02-11 06:35:35 +0800

    feat: update Application to enforce .txt extension and display 'Hash file saved as ...' message after progress bar completion

--- lib/sha256_everything/application.rb
+++ lib/sha256_everything/application.rb
@@ -3,22 +3,27 @@ require 'ruby-progressbar'
 module Sha256Everything
   class Application
     def initialize(options)
-      @directory   = options.fetch(:directory, Dir.pwd)
-      @output_file = options.fetch(:filename, "hashes.txt")
+      @directory = options.fetch(:directory, Dir.pwd)
+      file_name = options.fetch(:filename, "hashes.txt")
+      @output_file = file_name.end_with?(".txt") ? file_name : "#{file_name}.txt"
       @display     = options.fetch(:display_here, false)
     end
 
     def run
-      files     = FileCollector.new(@directory).files
-      progress  = ProgressBar.create(total: files.size, format: '%a %B %p%% %t')
-      results   = files.each_with_object({}) do |file, memo|
-                    memo[file] = FileHasher.new(file).compute
-                    progress.increment
-                  end
+      files = FileCollector.new(@directory).files
+      progress = ProgressBar.create(total: files.size, format: '%a %B %p%% %t')
+      results = files.each_with_object({}) do |file, memo|
+        memo[file] = FileHasher.new(file).compute
+        progress.increment
+      end
 
-      tree = TreeGenerator.new(@directory).generate
-      OutputWriter.new(@output_file, results, tree).write
-      puts "\nFile Hierarchy:" if @display
+      # Generate formatted tree (including hash values)
+      tree = TreeGenerator.new(@directory, results).generate
+      OutputWriter.new(@output_file, tree).write
+
+      # Display message after progress bar finishes
+      puts "\n\nHash file saved as #{@output_file}"
+      puts "\n" if @display
       puts tree if @display
     end
   end
