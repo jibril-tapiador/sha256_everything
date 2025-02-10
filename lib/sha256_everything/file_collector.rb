3e40b0fa1ac4c183fcaf520134f519464549ff07
Jibril Tapiador <tapiador@jib.is>
2025-02-11 07:15:01 +0800

    refactor: update FileCollector to include hidden files and exclude .git folder from processing

--- lib/sha256_everything/file_collector.rb
+++ lib/sha256_everything/file_collector.rb
@@ -1,11 +1,23 @@
 module Sha256Everything
   class FileCollector
+    IGNORED_DIRECTORIES = ['.git'].freeze
+
     def initialize(directory)
       @directory = directory
     end
 
     def files
-      Dir.glob(File.join(@directory, '**', '*')).select { |f| File.file?(f) }
+      pattern = File.join(@directory, '**', '{*,.*}')
+      Dir.glob(pattern, File::FNM_DOTMATCH)
+         .reject { |f| ignore?(f) }
+         .select { |f| File.file?(f) }
+    end
+
+    private
+
+    def ignore?(file)
+      path_components = file.split(File::SEPARATOR)
+      (path_components & IGNORED_DIRECTORIES).any?
     end
   end
 end
