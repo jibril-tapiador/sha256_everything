a018a349aaa7e786887481b1d35031fee87d5889
Jibril Tapiador <tapiador@jib.is>
2025-02-24 03:38:03 +0800

    refactor: simplify Application class by extracting logic into helper methods

--- lib/sha256_everything/application.rb
+++ lib/sha256_everything/application.rb
@@ -4,27 +4,57 @@ module Sha256Everything
   class Application
     def initialize(options)
       @directory = options.fetch(:directory, Dir.pwd)
-      file_name = options.fetch(:filename, "hashes.txt")
-      @output_file = file_name.end_with?(".txt") ? file_name : "#{file_name}.txt"
-      @display     = options.fetch(:display_here, false)
+      @output_file = format_filename(options.fetch(:filename, 'hashes.txt'))
+      @display = options.fetch(:display_here, false)
     end
 
     def run
-      files = FileCollector.new(@directory).files
-      progress = ProgressBar.create(total: files.size, format: '%a %B %p%% %t')
-      results = files.each_with_object({}) do |file, memo|
+      files = collect_files
+      results = hash_files(files)
+      tree = generate_tree(results)
+      write_output(tree)
+
+      display_progress_message
+      display_tree(tree) if @display
+    end
+
+    private
+
+    def format_filename(filename)
+      filename.end_with?('.txt') ? filename : "#{filename}.txt"
+    end
+
+    def collect_files
+      FileCollector.new(@directory).files
+    end
+
+    def hash_files(files)
+      progress = create_progress_bar(files)
+      files.each_with_object({}) do |file, memo|
         memo[file] = FileHasher.new(file).compute
         progress.increment
       end
+    end
 
-      # Generate formatted tree (including hash values)
-      tree = TreeGenerator.new(@directory, results).generate
+    def create_progress_bar(files)
+      ProgressBar.create(total: files.size, format: '%a %B %p%% %t')
+    end
+
+    def generate_tree(results)
+      TreeGenerator.new(@directory, results).generate
+    end
+
+    def write_output(tree)
       OutputWriter.new(@output_file, tree).write
+    end
 
-      # Display message after progress bar finishes
+    def display_progress_message
       puts "\n\nHash file saved as #{@output_file}"
-      puts "\n" if @display
-      puts tree if @display
+    end
+
+    def display_tree(tree)
+      puts "\n"
+      puts tree
     end
   end
 end
